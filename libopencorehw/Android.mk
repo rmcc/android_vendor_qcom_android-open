aa815577eb8b481f2d3c35f81ab4cf64ead0f14a
Vasantha Kumar <vasant@codeaurora.org>
2010-06-15 11:33:55 -0700

    vendor/qcom/android-open: Changes for adding new target msm7627_7x to build system
    Change-Id: I3d464aa91d9ce905c65ac4b453a38c2ded7e1c18

--- libopencorehw/Android.mk
+++ libopencorehw/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 include external/opencore/Config.mk
 LOCAL_C_INCLUDES := $(PV_INCLUDES)
 
-ifneq (, $(filter msm7201a_ffa msm7201a_surf msm7627_ffa msm7627_surf qsd8250_ffa qsd8250_surf, $(TARGET_PRODUCT)))
+ifneq (, $(filter msm7201a_ffa msm7201a_surf msm7627_ffa msm7627_surf msm7627_7x_ffa msm7627_7x_surf qsd8250_ffa qsd8250_surf, $(TARGET_PRODUCT)))
   LOCAL_SRC_FILES := android_surface_output_msm72xx.cpp
 endif
 ifneq (, $(filter msm7630_surf, $(TARGET_PRODUCT)))
